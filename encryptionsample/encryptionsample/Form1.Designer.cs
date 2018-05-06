4df91f3c727f43bdcd53faed9fa59f50292c93dc
softvernow <softvernow@softvernow.com>
2018-05-06 11:26:45 -0400

    encryptiondecryptionsample

---
+++ encryptionsample/encryptionsample/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace encryptionsample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtTextToBeEncrypted = new System.Windows.Forms.RichTextBox();
+            this.btnDecryptText = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEncryptedtext = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEncryptText = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(320, 343);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(126, 31);
+            this.btnClear.TabIndex = 17;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtTextToBeEncrypted
+            // 
+            this.txtTextToBeEncrypted.Location = new System.Drawing.Point(132, 9);
+            this.txtTextToBeEncrypted.Name = "txtTextToBeEncrypted";
+            this.txtTextToBeEncrypted.Size = new System.Drawing.Size(460, 139);
+            this.txtTextToBeEncrypted.TabIndex = 16;
+            this.txtTextToBeEncrypted.Text = "";
+            // 
+            // btnDecryptText
+            // 
+            this.btnDecryptText.Location = new System.Drawing.Point(171, 343);
+            this.btnDecryptText.Name = "btnDecryptText";
+            this.btnDecryptText.Size = new System.Drawing.Size(126, 31);
+            this.btnDecryptText.TabIndex = 15;
+            this.btnDecryptText.Text = "Start Decryption";
+            this.btnDecryptText.UseVisualStyleBackColor = true;
+            this.btnDecryptText.Click += new System.EventHandler(this.btnDecryptText_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(466, 343);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(126, 31);
+            this.btnExit.TabIndex = 14;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Encrypted Text:";
+            // 
+            // txtEncryptedtext
+            // 
+            this.txtEncryptedtext.Location = new System.Drawing.Point(132, 167);
+            this.txtEncryptedtext.Name = "txtEncryptedtext";
+            this.txtEncryptedtext.Size = new System.Drawing.Size(460, 152);
+            this.txtEncryptedtext.TabIndex = 12;
+            this.txtEncryptedtext.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Text To Be Encrypted:";
+            // 
+            // btnEncryptText
+            // 
+            this.btnEncryptText.Location = new System.Drawing.Point(15, 343);
+            this.btnEncryptText.Name = "btnEncryptText";
+            this.btnEncryptText.Size = new System.Drawing.Size(126, 31);
+            this.btnEncryptText.TabIndex = 10;
+            this.btnEncryptText.Text = "Start Encryption";
+            this.btnEncryptText.UseVisualStyleBackColor = true;
+            this.btnEncryptText.Click += new System.EventHandler(this.btnEncryptText_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 393);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtTextToBeEncrypted);
+            this.Controls.Add(this.btnDecryptText);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEncryptedtext);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEncryptText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.RichTextBox txtTextToBeEncrypted;
+        private System.Windows.Forms.Button btnDecryptText;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox txtEncryptedtext;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEncryptText;
+    }
+}
+
---
+++ encryptionsample/encryptionsample/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace encryptionsample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public String Encrypt(String data_to_encrypt)
+        {
+            //pattern of encryption
+            //you can change this pattern
+            String encryption_pattern = "your enc";
+
+            byte[] passwordBytes = UTF8Encoding.UTF8.GetBytes(encryption_pattern);
+            byte[] messageBytes = UTF8Encoding.UTF8.GetBytes(data_to_encrypt);
+
+            // Set encryption settings -- Use password for both key and init. vector     
+            DESCryptoServiceProvider provider = new DESCryptoServiceProvider();
+            provider.Mode = CipherMode.ECB;
+            provider.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform transform = provider.CreateEncryptor(passwordBytes, passwordBytes);
+            CryptoStreamMode mode = CryptoStreamMode.Write;
+
+            // Set up streams and encrypt     
+            MemoryStream memStream = new MemoryStream();
+            CryptoStream cryptoStream = new CryptoStream(memStream, transform, mode);
+            cryptoStream.Write(messageBytes, 0, messageBytes.Length);
+            cryptoStream.FlushFinalBlock();
+
+            // Read the encrypted message from the memory stream     
+            byte[] encryptedMessageBytes = new byte[memStream.Length];
+            memStream.Position = 0;
+            memStream.Read(encryptedMessageBytes, 0, encryptedMessageBytes.Length);
+
+            // Encode the encrypted message as base64 string     
+            String encryptedMessage = Convert.ToBase64String(encryptedMessageBytes);
+
+            //encrypted data
+            return encryptedMessage;
+
+        }
+
+        public String Decrypt(String encrypted_data)
+        {
+
+            encrypted_data = encrypted_data.Replace("\n", "");
+
+            // decrypt
+            // convert out of base 64
+            byte[] messageBytes = Convert.FromBase64String(encrypted_data);
+
+            //pattern of encryption
+            //you can change this pattern
+            //to decrypt your data, you need to use the same encryption pattern. 
+            String encryption_pattern = "your enc";
+
+            byte[] passwordBytes = UTF8Encoding.UTF8.GetBytes(encryption_pattern);
+
+            // Set encryption settings -- Use password for both key and init. vector     
+            DESCryptoServiceProvider provider = new DESCryptoServiceProvider();
+            provider.Mode = CipherMode.ECB;
+            provider.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform transform = provider.CreateDecryptor(passwordBytes, passwordBytes);
+            CryptoStreamMode mode = CryptoStreamMode.Write;
+
+            // Set up streams and decrypt    
+            MemoryStream memStream = new MemoryStream();
+            CryptoStream cryptoStream = new CryptoStream(memStream, transform, mode);
+            cryptoStream.Write(messageBytes, 0, messageBytes.Length);
+            cryptoStream.FlushFinalBlock();
+
+            // Read the decrypted message from the memory stream    
+            byte[] decryptedMessageBytes = new byte[memStream.Length];
+            memStream.Position = 0;
+            memStream.Read(decryptedMessageBytes, 0, decryptedMessageBytes.Length);
+
+            // Decode the decrypted message 
+            string decrypted_data = System.Text.UTF8Encoding.UTF8.GetString(decryptedMessageBytes);
+
+            //return decrypted data
+            return decrypted_data;
+
+        }
+
+        private void btnEncryptText_Click(object sender, EventArgs e)
+        {
+            //check if data is ready to be encrypted
+            if (txtTextToBeEncrypted.Lines.Length > 0)
+                txtEncryptedtext.Clear();
+
+            //loop through every line in the richtext box and encrypts line by line
+            for (int i = 0; i < txtTextToBeEncrypted.Lines.Length; i++)
+            {
+                //outputs encryption of each line to the bottom richtextbox. 
+                txtEncryptedtext.Text = txtEncryptedtext.Text + Encrypt(txtTextToBeEncrypted.Lines[i]) + "\n";
+            }
+        }
+
+        private void btnDecryptText_Click(object sender, EventArgs e)
+        {
+
+            //check if data is ready to be encrypted
+            if (txtEncryptedtext.Lines.Length > 0)
+                txtTextToBeEncrypted.Clear();
+
+            //loop through every line in the richtext box and encrypts line by line
+            for (int i = 0; i < txtEncryptedtext.Lines.Length; i++)
+            {
+                //outputs encryption of each line to the top richtextbox. 
+                txtTextToBeEncrypted.Text = txtTextToBeEncrypted.Text + Decrypt(txtEncryptedtext.Lines[i]) + "\n";
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtTextToBeEncrypted.Clear();
+            txtEncryptedtext.Clear();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
